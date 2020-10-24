1246a5f42503110f2192062b7eacea033878684a
Joey Jiao <joeyjiaojg@163.com>
2020-10-24 15:42:22 +0800

    Add AFL remote code fuzzing libraries

---
+++ remote_process/aflclient.c
@@ -0,0 +1,152 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <signal.h>
+#include "../android-ashmem.h"
+
+#define MAP_SIZE_POW2 16
+#define MAP_SIZE (1 << MAP_SIZE_POW2)
+
+#define SHM_ENV_VAR         "__AFL_SHM_ID"
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+static s32 shm_id;
+static s32 server_pid = -1;
+static s32 client_pid = -1;
+char* tmpdir;
+
+u8  __afl_area_initial[MAP_SIZE];
+u8* __afl_area_ptr = __afl_area_initial;
+u8* trace_bits = __afl_area_initial;
+
+int fd_fifo_ctl;
+int fd_fifo_st;
+static int status;
+
+void handle_sig(int sig) {
+  if (sig == 9 || sig == 15) {
+    if (server_pid != -1)
+      kill(server_pid, sig);
+  }
+
+  _exit(sig);
+}
+
+void setup_signal_handlers(void) {
+  struct sigaction sa;
+
+  sa.sa_handler = NULL;
+  sa.sa_flags = SA_RESTART;
+  sa.sa_sigaction = NULL;
+
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = handle_sig;
+
+  sigaction(SIGTERM, &sa, NULL);
+  sigaction(SIGKILL, &sa, NULL);
+}
+
+/* SHM setup. */
+
+void __afl_map_shm(void) {
+
+  char *id_str = getenv(SHM_ENV_VAR);
+
+  /* If we're running under AFL, attach to the appropriate region, replacing the
+     early-stage __afl_area_initial region that is needed to allow some really
+     hacky .init code to work correctly in projects such as OpenSSL. */
+
+  if (id_str) {
+
+    shm_id = atoi(id_str);
+
+    __afl_area_ptr = shmat(shm_id, NULL, 0);
+
+    /* Whooooops. */
+
+    if (__afl_area_ptr == (void *)-1) _exit(1);
+
+    /* Write something into the bitmap so that even with low AFL_INST_RATIO,
+       our parent doesn't give up on us. */
+
+    __afl_area_ptr[0] = 1;
+
+  }
+
+}
+
+void __afl_start_client(void) {
+  tmpdir = getenv("TMPDIR");
+  if (!tmpdir) tmpdir = "/tmp";
+
+  char fifo_ctl[1024];
+  char fifo_st[1024];
+  snprintf(fifo_ctl, sizeof(fifo_ctl), "%s/fifo_ctl", tmpdir);
+  snprintf(fifo_st, sizeof(fifo_st), "%s/fifo_st", tmpdir);
+
+  if (access(fifo_ctl, F_OK) != 0) {
+    if (mkfifo(fifo_ctl, 0666) < 0) _exit(1);
+  }
+
+  if (access(fifo_st, F_OK) != 0) _exit(1);
+
+  if ((fd_fifo_st=open(fifo_st, O_RDONLY)) < 0) _exit(1);
+
+  if ((fd_fifo_ctl=open(fifo_ctl, O_WRONLY)) < 0) _exit(1);
+
+  client_pid = getpid();
+  if (write(fd_fifo_ctl, &client_pid, 4) != 4) _exit(1);
+
+  if (read(fd_fifo_st, &server_pid, 4) != 4) _exit(1);
+
+  if (write(fd_fifo_ctl, &shm_id, 4) != 4) _exit(1);
+
+}
+
+void afl_client_init(void) {
+
+  static u8 init_done;
+
+  if (!init_done) {
+
+    __afl_map_shm();
+    __afl_start_client();
+    init_done = 1;
+
+  }
+
+}
+
+__attribute__((constructor(5)))
+void afl_client_setup(void) {
+  setup_signal_handlers();
+
+  afl_client_init();
+}
+
+__attribute__((destructor))
+void afl_client_exit(void) {
+
+  if (read(fd_fifo_st, &status, 4) != 4) _exit(1);
+
+  close(fd_fifo_st);
+  close(fd_fifo_ctl);
+
+  _exit(status);
+}
---
+++ remote_process/aflserver.c
@@ -0,0 +1,245 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <signal.h>
+#include "../android-ashmem.h"
+
+#define MAP_SIZE_POW2 16
+#define MAP_SIZE (1 << MAP_SIZE_POW2)
+
+#define SHM_ENV_VAR         "__AFL_SHM_ID"
+#define MAX_ALLOC           0x40000000
+
+#define SAYF(x...)    printf(x)
+
+#define PFATAL(x...) do { \
+    fflush(stdout); \
+    SAYF("\n[-]  SYSTEM ERROR : " x); \
+    SAYF("\n    Stop location : %s(), %s:%u\n", \
+         __FUNCTION__, __FILE__, __LINE__); \
+    SAYF("       OS message : %s\n", strerror(errno)); \
+    exit(1); \
+  } while (0)
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+static s32 shm_id = -1;
+static s32 server_pid = -1,
+           client_pid = -1;
+static u8* trace_bits;
+char* tmpdir;
+
+#define ALLOC_MAGIC_C1  0xFF00FF00 /* Used head (dword)  */
+#define ALLOC_MAGIC_F   0xFE00FE00 /* Freed head (dword) */
+#define ALLOC_MAGIC_C2  0xF0       /* Used tail (byte)   */
+
+#define ALLOC_C1(_ptr)  (((u32*)(_ptr))[-2])
+#define ALLOC_S(_ptr)   (((u32*)(_ptr))[-1])
+#define ALLOC_C2(_ptr)  (((u8*)(_ptr))[ALLOC_S(_ptr)])
+
+#define ALLOC_OFF_HEAD  8
+#define ALLOC_OFF_TOTAL (ALLOC_OFF_HEAD + 1)
+
+#define ABORT(x...) do { \
+    SAYF("\n[-] PROGRAM ABORT : " x); \
+    SAYF("\n    Stop location : %s(), %s:%u\n\n", \
+         __FUNCTION__, __FILE__, __LINE__); \
+    abort(); \
+  } while (0)
+
+#define FATAL(x...) do { \
+    SAYF("\n[-] PROGRAM ABORT : " x); \
+    SAYF("\n         Location : %s(), %s:%u\n\n", \
+         __FUNCTION__, __FILE__, __LINE__); \
+    exit(1); \
+  } while (0)
+
+#define CHECK_PTR(_p) do { \
+    if (_p) { \
+      if (ALLOC_C1(_p) ^ ALLOC_MAGIC_C1) {\
+        if (ALLOC_C1(_p) == ALLOC_MAGIC_F) \
+          ABORT("Use after free."); \
+        else ABORT("Corrupted head alloc canary."); \
+      } \
+      if (ALLOC_C2(_p) ^ ALLOC_MAGIC_C2) \
+        ABORT("Corrupted tail alloc canary."); \
+    } \
+  } while (0)
+
+#define ALLOC_CHECK_SIZE(_s) do { \
+    if ((_s) > MAX_ALLOC) \
+      ABORT("Bad alloc request: %u bytes", (_s)); \
+  } while (0)
+
+#define ALLOC_CHECK_RESULT(_r, _s) do { \
+    if (!(_r)) \
+      ABORT("Out of memory: can't allocate %u bytes", (_s)); \
+  } while (0)
+
+static inline void* DFL_ck_alloc_nozero(u32 size) {
+
+  void* ret;
+
+  if (!size) return NULL;
+
+  ALLOC_CHECK_SIZE(size);
+  ret = malloc(size + ALLOC_OFF_TOTAL);
+  ALLOC_CHECK_RESULT(ret, size);
+
+  ret += ALLOC_OFF_HEAD;
+
+  ALLOC_C1(ret) = ALLOC_MAGIC_C1;
+  ALLOC_S(ret)  = size;
+  ALLOC_C2(ret) = ALLOC_MAGIC_C2;
+
+  return ret;
+
+}
+
+static inline void* DFL_ck_alloc(u32 size) {
+
+  void* mem;
+
+  if (!size) return NULL;
+  mem = DFL_ck_alloc_nozero(size);
+
+  return memset(mem, 0, size);
+
+}
+
+#define ck_alloc          DFL_ck_alloc
+#define ck_free           DFL_ck_free
+
+static inline void DFL_ck_free(void* mem) {
+
+  if (!mem) return;
+
+  CHECK_PTR(mem);
+
+#ifdef DEBUG_BUILD
+
+  /* Catch pointer issues sooner. */
+  memset(mem, 0xFF, ALLOC_S(mem));
+
+#endif /* DEBUG_BUILD */
+
+  ALLOC_C1(mem) = ALLOC_MAGIC_F;
+
+  free(mem - ALLOC_OFF_HEAD);
+
+}
+
+#define alloc_printf(_str...) ({ \
+    u8* _tmp; \
+    s32 _len = snprintf(NULL, 0, _str); \
+    if (_len < 0) FATAL("Whoa, snprintf() fails?!"); \
+    _tmp = ck_alloc(_len + 1); \
+    snprintf((char*)_tmp, _len + 1, _str); \
+    _tmp; \
+  })
+
+void setup_shm(void) {
+  char* shm_str;
+
+  if (shm_id != -1) {
+
+    shm_str = (char*)alloc_printf("%d", shm_id);
+    if (!getenv("AFL_DUMB_MODE")) setenv(SHM_ENV_VAR, shm_str, 1);
+
+    ck_free(shm_str);
+  } else {
+    shm_str = getenv(SHM_ENV_VAR);
+    if (!shm_str) _exit(1);
+    shm_id = atoi(shm_str);
+  }
+
+  trace_bits = shmat(shm_id, NULL, 0);
+
+  if (trace_bits == (void*)-1) PFATAL("shmat() failed");
+}
+
+int fd_fifo_ctl;
+int fd_fifo_st;
+
+void handle_sig(int sig) {
+  if (sig == 6 || sig == 8) {
+    if (client_pid != -1)
+      kill(client_pid, sig);
+  }
+
+  exit(sig);
+}
+
+void setup_signal_handlers(void) {
+  struct sigaction sa;
+
+  sa.sa_handler = NULL;
+  sa.sa_flags = SA_RESTART;
+  sa.sa_sigaction = NULL;
+
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = handle_sig;
+
+  sigaction(SIGABRT, &sa, NULL);
+  sigaction(SIGFPE, &sa, NULL);
+  sigaction(SIGALRM, &sa, NULL);
+}
+
+void afl_server_exit(int status, void* arg);
+
+__attribute__((constructor))
+void setup_afl_server() {
+  on_exit(afl_server_exit, "afl_server_exit");
+
+  setup_signal_handlers();
+
+  tmpdir = getenv("TMPDIR");
+  if (!tmpdir) tmpdir = "/tmp";
+
+  char fifo_ctl[1024];
+  char fifo_st[1024];
+  snprintf(fifo_ctl, sizeof(fifo_ctl), "%s/fifo_ctl", tmpdir);
+  snprintf(fifo_st, sizeof(fifo_st), "%s/fifo_st", tmpdir);
+
+  if (access(fifo_st, F_OK) != 0) {
+    if (mkfifo(fifo_st, 0666) < 0) exit(1);
+  }
+
+  if ((fd_fifo_st=open(fifo_st, O_WRONLY)) < 0) exit(1);
+
+  if ((fd_fifo_ctl=open(fifo_ctl, O_RDONLY)) < 0) exit(1);
+
+  if (read(fd_fifo_ctl, &client_pid, 4) != 4) exit(1);
+
+  server_pid = getpid();
+  if (write(fd_fifo_st, &server_pid, 4) != 4) exit(1);
+
+  if (read(fd_fifo_ctl, &shm_id, 4) != 4) _exit(1);
+
+  setup_shm();
+}
+
+void afl_server_exit(int status, void* arg) {
+
+  if (write(fd_fifo_st, &status, 4) != 4) _exit(1);
+
+  close(fd_fifo_st);
+  close(fd_fifo_ctl);
+
+  _exit(status);
+}
