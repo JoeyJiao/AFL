54ae80b5b7ae5bdf310b23d7fe060a9a585f05e1
Jiangen Jiao <joeyjiaojg@163.com>
2021-04-07 11:36:22 +0800

    android: hack for build

--- llvm_mode/afl-clang-fast.c
+++ llvm_mode/afl-clang-fast.c
@@ -129,8 +129,8 @@ static void edit_params(u32 argc, char** argv) {
 #ifdef USE_TRACE_PC
   cc_params[cc_par_cnt++] = "-fsanitize-coverage=trace-pc-guard";
 #ifndef __ANDROID__
-  cc_params[cc_par_cnt++] = "-mllvm";
-  cc_params[cc_par_cnt++] = "-sanitizer-coverage-block-threshold=0";
+//  cc_params[cc_par_cnt++] = "-mllvm";
+//  cc_params[cc_par_cnt++] = "-sanitizer-coverage-block-threshold=0";
 #endif
 #else
   cc_params[cc_par_cnt++] = "-Xclang";
